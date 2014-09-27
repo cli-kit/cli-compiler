04319356444ae0ce244263e6af036f5b9ae84477
cyberfunk <hello@cyberfunk.org>
2014-09-28 02:38:56 +0800

    Initial files.

---
+++ doc/readme/introduction.md
@@ -0,0 +1,1 @@
+Compiler for the [command][] module, a component of the [toolkit][].
---
+++ test/spec/compiler.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+var compiler = require('../..');
+
+describe('cli-compiler:', function() {
+  it('should compile program', function(done) {
+    done();
+  });
+})
