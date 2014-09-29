a952a261c15f72cd502e5227a0dce65447f9f8f1
cyberfunk <hello@cyberfunk.org>
2014-09-29 15:45:24 +0800

    Move deprecated modules to legacy object.

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
 var loader = require('./lib/deprecated/loader');
 var substitutor = require('./lib/deprecated/substitute');
 
-function deprecated(msg, parameters) {
-  console.warn(msg, parameters || []);
-}
-
-module.exports = {
-  load: function() {
-    deprecated('load middleware is deprecated, please use compiler');
-    loader.apply(this, arguments);
-  },
-  substitute: function() {
-    deprecated('substitute middleware is deprecated, please use compiler');
-    substitute.apply(this, arguments);
-  }
-}
-
-module.exports.compiler = require('./lib');
+module.exports = require('./lib');
 module.exports.load = require('./lib/load');
 module.exports.ConverterMap = require('./lib/converter-map');
+
+module.exports.legacy = {
+  load: loader,
+  substitute: substitutor
+}
