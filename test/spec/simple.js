894d4d77cf02ba2b32b152cd5bbfa77846f5723b
cyberfunk <hello@cyberfunk.org>
2014-09-29 22:26:51 +0800

    Tidy test assertions, assert on cache load.

--- test/spec/simple.js
+++ test/spec/simple.js
@@ -24,77 +24,81 @@ function subcommand(sub) {
   expect(sub.key()).to.eql('mockSubCommand');
 }
 
+function assert(prg) {
+  expect(prg).to.be.an('object');
+  expect(prg).to.be.an.instanceof(Program);
+
+  expect(prg.name()).to.eql('simple-mock-program');
+  expect(prg.version()).to.eql('1.0.0');
+
+  var des = prg.description();
+  expect(des).to.be.an('object');
+  expect(des.txt).to.eql('Simple program.');
+  expect(des.md).to.eql('*Simple* program.');
+
+  var det = prg.detail();
+  expect(det).to.be.an('object');
+  expect(det.txt).to.eql('Mock detail about the program.');
+  expect(det.md).to.eql('Mock `detail` about the program.');
+
+  var opts = prg.options()
+    , cmds = prg.commands();
+
+  expect(opts).to.be.an('object');
+  expect(cmds).to.be.an('object');
+
+  expect(Object.keys(opts)).to.eql(['mockOption', 'mockFlag']);
+  expect(Object.keys(cmds)).to.eql(['mockCommand']);
+
+  // basic test on the compiled prg (in-memory)
+  var mockOption = prg.options().mockOption;
+  expect(mockOption).to.be.an('object');
+  expect(mockOption.key()).to.eql('mockOption');
+  expect(mockOption.names()).to.eql(['-o', '--mock-option']);
+
+  converter(mockOption.converter());
+  sections(prg.sections());
+
+  var mockCommand = prg.commands().mockCommand;
+  var mockSubCommand = mockCommand.commands().mockSubCommand;
+  subcommand(mockSubCommand);
+}
+
 describe('cli-compiler:', function() {
 
   it('should compile simple program', function(done) {
     var opts = mock.opts.simple;
     compiler(opts, function(err, req) {
-      //console.dir(err);
       expect(err).to.eql(undefined);
       expect(req).to.be.an('object');
+      expect(req.program).to.be.an('object');
 
-      var program = req.program;
-
-      expect(program).to.be.an('object');
-      expect(program.options()).to.be.an('object');
-      expect(program.commands()).to.be.an('object');
-
-      // basic test on the compiled program (in-memory)
-      var mockOption = program.options().mockOption;
-      expect(mockOption).to.be.an('object');
-      expect(mockOption.key()).to.eql('mockOption');
-      expect(mockOption.names()).to.eql(['-o', '--mock-option']);
-
-      converter(mockOption.converter());
-      sections(program.sections());
-
-      var mockCommand = program.commands().mockCommand;
-      var mockSubCommand = mockCommand.commands().mockSubCommand;
-      subcommand(mockSubCommand);
+      assert(req.program);
 
       // run an empty program against the compiled
       // closure loaded from disc
       mock.run(opts, function(err, prg) {
-
         expect(err).to.eql(null);
         expect(prg).to.be.an('object');
-        expect(prg).to.be.an.instanceof(Program);
-        expect(prg.name()).to.eql('simple-mock-program');
-        expect(prg.version()).to.eql('1.0.0');
 
-        var des = prg.description();
-        expect(des).to.be.an('object');
-        expect(des.txt).to.eql('Simple program.');
-        expect(des.md).to.eql('*Simple* program.');
-
-        var det = prg.detail();
-        expect(det).to.be.an('object');
-        expect(det.txt).to.eql('Mock detail about the program.');
-        expect(det.md).to.eql('Mock `detail` about the program.');
-
-        var opts = prg.options()
-          , cmds = prg.commands();
-
-        expect(opts).to.be.an('object');
-        expect(cmds).to.be.an('object');
-
-        expect(Object.keys(opts)).to.eql(['mockOption', 'mockFlag']);
-        expect(Object.keys(cmds)).to.eql(['mockCommand']);
-
-        expect(cmds.mockCommand).to.be.instanceof(Command);
-        expect(opts.mockOption).to.be.instanceof(Option);
-        expect(opts.mockFlag).to.be.instanceof(Flag);
-
-        converter(opts.mockOption.converter());
-        // loaded sections information
-        sections(prg.sections());
-
-        var mockCommand = prg.commands().mockCommand;
-        var mockSubCommand = mockCommand.commands().mockSubCommand;
-        //subcommand(mockCommand.commands());
+        assert(prg);
 
         done();
       });
+    });
+  });
+
+
+  it('should load simple program from cache', function(done) {
+    var opts = mock.opts.simple;
+    compiler(opts, function(err, req) {
+      expect(err).to.eql(undefined);
+      expect(req).to.be.an('object');
+      expect(req.program).to.be.an('object');
+
+      assert(req.program);
+
+      done();
     });
   });
 })
