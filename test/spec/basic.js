b7f1e911f669e2c29791d416c730b3f5165f0c0a
cyberfunk <hello@cyberfunk.org>
2014-09-30 15:11:39 +0800

    Implement basic program test fixture.
    Ensures compiler name, version and description options are respected.

--- test/spec/basic.js
+++ test/spec/basic.js
@@ -4,18 +4,22 @@ var mock = require('../util/mock');
 
 describe('cli-compiler:', function() {
   it('should compile basic program', function(done) {
-    var opts = mock.opts.empty;
+    var opts = mock.opts.basic;
     compiler(opts, function(err, req) {
       expect(req).to.be.an('object');
       expect(req.program).to.be.an('object');
-      //expect(req.program.options()).to.be.an('object');
-      //expect(req.program.commands()).to.be.an('object');
-      //expect(Object.keys(req.program.options())).to.eql([]);
-      //expect(Object.keys(req.program.commands())).to.eql([]);
-      //expect(req.program.name()).to.eql('program');
-      //expect(req.program.version()).to.eql('0.0.1');
-      //expect(req.program.description().txt).to.eql('Empty program.');
-      //expect(req.program.description().md).to.eql('*Empty* program.');
+      expect(req.program.options()).to.be.an('object');
+      expect(req.program.commands()).to.be.an('object');
+      expect(Object.keys(req.program.options())).to.eql(
+        ['mockOption', 'mockFlag']);
+      expect(Object.keys(req.program.commands())).to.eql(
+        ['mockCommand']);
+      expect(req.program.name()).to.eql('basic-program');
+      expect(req.program.version()).to.eql('1.0.0');
+      expect(req.program.description().txt).to.eql(
+        'A basic program description');
+      expect(req.program.description().md).to.eql(
+        'A *basic* program description');
       done();
     });
   });
