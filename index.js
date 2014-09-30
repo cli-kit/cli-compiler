657ca39d0a6fb0c751dd7ec5ab9141a0e3dbc5bf
cyberfunk <hello@cyberfunk.org>
2014-09-30 14:10:11 +0800

    Use converter-util.

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var substitutor = require('./lib/deprecated/substitute');
 
 module.exports = require('./lib');
 module.exports.load = require('./lib/load');
-module.exports.ConverterMap = require('./lib/converter-map');
 
 module.exports.legacy = {
   load: loader,
