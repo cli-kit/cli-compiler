0f9588b23fbf78429b7361ff27401743aee3394e
cyberfunk <hello@cyberfunk.org>
2014-10-01 01:25:50 +0800

    Remove obsolete functions.

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-var loader = require('./lib/deprecated/loader');
-var substitutor = require('./lib/deprecated/substitute');
-
 module.exports = require('./lib');
 module.exports.load = require('./lib/load');
-
-module.exports.legacy = {
-  load: loader,
-  substitute: substitutor
-}
